3c37efb2d7d1034dd1b3fb8ae9f71d2fe3448965
Reza Hemmati <50717617+QuChem@users.noreply.github.com>
2022-12-15 13:35:05 -0600

    Update cosmo.F90
    removed unused variables

--- src/solvation/cosmo.F90
+++ src/solvation/cosmo.F90
@@ -641,34 +641,13 @@ subroutine coscl1 (a, id, n)
     integer, intent (in) :: n
     double precision, dimension (*), intent (inout) :: a
     integer, dimension (n), intent (inout) :: id
-    integer :: i, indi, indk, j, k, kk, info
-    double precision :: summe
+    integer :: i, indi, info
     indi = 0
     do i = 1, n
       id(i) = indi
       indi = indi + i
     end do
     call dpotrf('L', n, a, n, info)
-    !do k = 1, n
-    !  indk = id(k)
-    !  kk = k + indk
-    !  do i = k, n
-    !    indi = id(i)
-    !    summe = 0.d0
-    !    do j = 1, k - 1
-    !      summe = summe + a(j + indi) * a(j + indk)
-    !    end do
-    !    summe = a(k + indi) - summe
-    !    if (i == k) then
-    !      if (summe < 0.0d0) then
-    !        summe = a(kk)
-    !      end if
-    !      a(kk) = 1.d0 / Sqrt (summe)
-    !    else
-    !      a(k + indi) = summe * a(kk)
-    !    end if
-    !  end do
-    !end do
 end subroutine coscl1
 subroutine coscl2 (a, id, x, y, n)
    ! THIS ROUTINE SOLVES THE LINEAR SYSTEM CX = Y BASED ON
