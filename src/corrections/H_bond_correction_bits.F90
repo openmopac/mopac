03f0fa53d357a3d7c89141963683196f3a1b9159
Jonathan Moussa <jemoussa@vt.edu>
2024-12-10 15:31:23 -0500

    Ignore single-character labels when printing H bonds

--- src/corrections/H_bond_correction_bits.F90
+++ src/corrections/H_bond_correction_bits.F90
@@ -356,7 +356,7 @@ subroutine find_H__Y_bonds(acc_a, nacc_a, acc_b, nacc_b, bonding_a_h, nb_a_h, hb
     if (txtatm(D) == " ") write(txtatm(D),'(a,i5,3x,a)')"Atom No.:", D, elemnt(nat(D))
     if (txtatm(H) == " ") write(txtatm(H),'(a,i5,3x,a)')"Atom No.:", H, elemnt(nat(H))
     if (txtatm(A) == " ") write(txtatm(A),'(a,i5,3x,a)')"Atom No.:", A, elemnt(nat(A))
-    if (maxtxt == 0) then
+    if (maxtxt < 2) then
       write(H_txt(P_Hbonds),'(2x,a,f15.3,6x,a,12x,a,10x, f7.2,a)')trim(txtatm(D)), sum1,  &
       trim(txtatm(H)), trim(txtatm(A)), energy, " Kcal/mol"
     else
