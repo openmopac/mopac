92b31dd0bcece4d2baa2b94eb31a853a785fcbe7
Jonathan Moussa <jemoussa@vt.edu>
2025-02-05 01:27:38 -0500

    Divide & conquer to find crashing iteration (5000)

--- src/input/getgeo.F90
+++ src/input/getgeo.F90
@@ -621,7 +621,7 @@ write(*,*) "before main loop of getgeo"
         return
       end if
 write(*,*) "loop iter", natoms
-if(natoms > 4500) stop
+if(natoms > 5000) stop
       go to 20
 !***********************************************************************
 ! ALL DATA READ IN, CLEAN UP AND RETURN
